815db77e82211eb35426d824dabe3ad2a97f6b11
lizza <liutao_glory@126.com>
2021-03-20 10:41:52 +0800

    lizza commit.

---
+++ java-container/src/main/java/com/lizza/Map/HashMap/Map_3.java
@@ -0,0 +1,18 @@
+package com.lizza.Map.HashMap;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @Desc:
+ * @author: lizza.liu
+ * @date: 2021-03-19
+ */
+public class Map_3 {
+
+    public static void main(String[] args){
+        for (int i = 0; i < 5000; i++) {
+
+        }
+    }
+}
