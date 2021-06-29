b9a8f400cf0762920edf86bd2165891f6db44e94
lizza <liutao_glory@126.com>
2021-06-29 10:48:16 +0800

    并行流

--- java-container/src/main/java/com/lizza/Set/Guava_Set.java
+++ java-container/src/main/java/com/lizza/Set/Guava_Set.java
@@ -2,23 +2,19 @@ package com.lizza.Set;
 
 import com.google.common.collect.Sets;
 
-import java.time.LocalDateTime;
 import java.util.HashSet;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 /**
- * @Desc:
- * @author: lizza.liu
- * @date: 2021-01-27
+ * 集合复制
  */
 public class Guava_Set {
 
     public static void main(String[] args){
-        long now = System.currentTimeMillis();
-        for (int i = 0; i < 100000; i++) {
-            Set<String> set = new HashSet<>();
+        Set<String> set = new HashSet<>();
+        for (int i = 0; i < 100; i++) {
+            set.add("e" + i);
         }
-        System.out.println((System.currentTimeMillis() - now));
+        Sets.newHashSet(set);
     }
 }
